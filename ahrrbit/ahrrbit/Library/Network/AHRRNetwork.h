3f65fa06b0e6931ba047e189a4c16bf146457ef1
Massimo Oliviero <massimo.oliviero@gmail.com>
2013-12-20 10:56:05 +0100

    networking

---
+++ ahrrbit/ahrrbit/Library/Network/AHRRNetwork.h
@@ -0,0 +1,13 @@
+//
+//  AHRRNetwork.h
+//  ahrrbit
+//
+//  Created by Massimo Oliviero on 20/12/13.
+//  Copyright (c) 2013 noleved. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AHRRNetwork : NSObject
+
+@end
---
+++ ahrrbit/ahrrbit/Library/Network/AHRRNetwork.m
@@ -0,0 +1,15 @@
+//
+//  AHRRNetwork.m
+//  ahrrbit
+//
+//  Created by Massimo Oliviero on 20/12/13.
+//  Copyright (c) 2013 noleved. All rights reserved.
+//
+
+#import "AHRRNetwork.h"
+
+#import <RestKit/RestKit.h>
+
+@implementation AHRRNetwork
+
+@end
