53849ea109006eddc6ad5520aec30b40c3fb4408
Massimo Oliviero <massimo.oliviero@gmail.com>
2013-12-20 10:24:54 +0100

    added workspace

---
+++ ahrrbit/ahrrbit/AHRRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AHRRAppDelegate.h
+//  ahrrbit
+//
+//  Created by Massimo Oliviero on 20/12/13.
+//  Copyright (c) 2013 noleved. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AHRRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
